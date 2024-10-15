568529f0268ef4369a2d99ee741930d085ebc43b
n1tr0x0 <isabaevali78@gmail.com>
2024-10-15 20:02:50 +0300

    jlwe

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from "react";
+import { RouterProvider } from "react-router-dom";
+import router from "./routing/Routing";
+import "./styles/App.scss";
 
 function App() {
   return (
-    
-  )
+    <div className="app">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
