7517270e30cc391708ab467de31d20580a0e7c76
khodzz <khodzhaevislambek@gmail.com>
2024-10-15 19:52:24 +0300

    fonts

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { RouterProvider } from "react-router-dom";
 import router from "./routing/Routing";
+import "./styles/App.scss";
 
 function App() {
   return (
--- src/routing/Routing.jsx
+++ src/routing/Routing.jsx
@@ -3,6 +3,8 @@ import { createBrowserRouter } from "react-router-dom";
 import Root from "../components/Root/Root";
 import Home from "../pages/Home/Home";
 import Cart from "../pages/Cart/Cart";
+import Login from "../pages/Login/Login";
+import Registration from "../pages/Registration/Registration";
 
 const router = createBrowserRouter([
   {
@@ -16,6 +18,14 @@ const router = createBrowserRouter([
       {
         path: "cart",
         element: <Cart />,
+      },
+      {
+        path: "login",
+        element: <Login />,
+      },
+      {
+        path: "registration",
+        element: <Registration />,
       },
     ],
   },
