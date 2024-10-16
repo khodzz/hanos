5ea8d227a6db3741ea9b512b20c3d2b318ebb27e
n1tr0x0 <isabaevali78@gmail.com>
2024-10-16 19:39:06 +0300

    header&footer

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
 import "./header.scss";
+import logo from "../../assets/logo.png"
+import user from "../../assets/user-ico.png"
+import locationImg from "../../assets/location-point.svg"
 
 const Header = () => {
   // состояние для отслеживания, открыто ли меню
@@ -10,11 +13,14 @@ const Header = () => {
     setIsOpen(!isOpen);
   };
 
+  const options = [
+    ''
+  ]
 
   return (
     <header className='header'>
       <div className="container">
-        <nav className="nav">
+        <nav className="nav-top">
           <div className="header__nav_left-side">
             <ul className='first-ul'>
               <li className="header__nav_link"><a href="">Over HANOS</a></li>
@@ -50,10 +56,63 @@ const Header = () => {
             <ul className="header__nav_right-side_ul">
               <li>Klantenservice</li>
               <li>Inloggen</li>
-              <li>nl</li>
+              <li>
+                <label className='nl'>
+                  <select name="selectedFruit">
+                    <option value="nl">nl</option>
+                    <option value="en">en</option>
+                    <option value="dt">dt</option>
+                  </select>
+                </label>
+              </li>
             </ul>
           </div>
         </nav>
+      </div>
+      <div className="top">
+        <div className="container">
+
+            <div className="logo-searcher">
+              <img className='logo' src={logo} alt="" />
+              <label className='search_btn'>
+                <input className='search' type="search" placeholder='Vind uw product' /><button className='search-btn'>Zoeken</button>
+              </label>
+            </div>
+
+
+            <div className="info">
+              <div className="locations">
+                <img className='location__img' src={locationImg} alt="" />
+                <a className='location__link' href="">Groothandels
+                <span className='location__link_span'>Vind uw groothande</span></a>
+              </div>
+
+              <div className="add-admin">
+                <img className='user-admin__img' src={user} alt="" />
+                <a className='user-admin-add_link' href="">Klant worden <span className='user-admin-add_link_span'>Registreer u vandaag</span> </a>
+              </div>
+            </div>
+        </div>
+      </div>
+      <div className="container contaner-bottom">
+          <ul className='ul_bottom'>
+              <li id='first' className="li_bottom">Acties
+              </li>
+              <li className="li_bottom">AGF</li>
+              <li className="li_bottom">Convenience</li>
+              <li className="li_bottom">Diepvrier</li>
+              <li className="li_bottom">Dranken</li>
+              <li className="li_bottom">DKW</li>
+              <li className="li_bottom">Kaas</li>
+              <li className="li_bottom">Non-food</li>
+              <li className="li_bottom">Patisserie</li>
+              <li className="li_bottom">Vis</li>
+              <li className="li_bottom">Vlees</li>
+              <li className="li_bottom">Wild & Gevogelte</li>
+              <li className="li_bottom">Zuivel & ei</li>
+              <li className="li_bottom">Uitgelicht</li>
+              <li className="li_bottom">FoodXperience</li>
+          </ul>
       </div>
     </header>
   );
